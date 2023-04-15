73d95d9c56dce60c6a1c43592db809669dc90172
cr4r <53891732+cr4r@users.noreply.github.com>
2023-04-15 15:44:18 +0700

    Update nodejs.sh
    update node to 19

--- installer/nodejs.sh
+++ installer/nodejs.sh
@@ -4,7 +4,7 @@
 [[ $(dpkg --get-selections | grep -w "g++" | head -1) ]] || (msg -ama "(1) Menginstall Module ..." && apt-get install g++ -y &>/dev/null && tput cuu1 && tput dl1)
 
 msg -ama "Instalasi Nodejs Dimulai"
-bash <(curl -L -s https://deb.nodesource.com/setup_18.x)
+bash <(curl -L -s https://deb.nodesource.com/setup_19.x)
 [[ $(dpkg --get-selections | grep -w "nodejs" | head -1) ]] || (msg -ama "(1) Menginstall Module ..." && apt-get install nodejs -y &>/dev/null && tput cuu1 && tput dl1)
 msg -ama "Instalasi Nodejs Selesai"
 msg -ama "Versi NodeJS $(node -v)"
